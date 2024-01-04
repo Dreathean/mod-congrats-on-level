105a47cfc6942e7fa75e583c0d277a2d2ff9aca3
Dreathean <132776330+Dreathean@users.noreply.github.com>
2024-01-04 16:38:27 -0500

    Update col_reward_items.sql

--- data/sql/db-world/col_reward_items.sql
+++ data/sql/db-world/col_reward_items.sql
@@ -13,7 +13,7 @@ DELETE FROM `mod_congrats_on_level_items`;
 INSERT INTO `mod_congrats_on_level_items` (`level`, `money`, `spell`, `learn`, `itemId1`, `itemId2`, `race`, `class`) VALUES
 (10, 1, 20217, 0, 3419, 18587, 0, 0), --red rose, goblin jumper cables xl
 (20, 10, 20217, 0, 49040, 11110, 0, 0), --jeeves, chicken egg pet
-(30, 15, 20217, 0, 18231, 46780, 0, 0),
+(30, 15, 20217, 0, 8491, 46780, 0, 0), -- black tabby pet, 
 (40, 20, 20217, 0, 3427, 49704, 0, 0), -- stylish black shirt, ??
 (50, 25, 20217, 0, 11000, 12382, 0, 0), --shadowforge key, strath key
 (60, 30, 20217, 0, 13704, 12344, 0, 0), --scholo key, seal of ascension
